7ca8f74c0263c412a36f46bd875bff0b43bf7f57
Ashrit Yarava <ashdragon95@gmail.com>
2022-04-01 15:17:04 -0400

    tempo stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    
+    // The output directory for the song files.
+    #[clap(short, long)]
+    output: Option<String>,
+
+    // The playlist to get the songs from.
+    playlist_url: Option<String>
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args.output.as_deref());
+    println!("{:?}", args.playlist_url.as_deref());
+
 }
