60ab7dd91f3abd06ffb3750b9fbecf0c7e155f32
Ashrit Yarava <ashdragon95@gmail.com>
2022-04-02 22:27:16 -0400

    Change order for song file name.

--- src/spotify.rs
+++ src/spotify.rs
@@ -15,7 +15,7 @@ pub struct Song {
 
 impl fmt::Display for Song {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} - {}", self.title, self.artists.join(", "))
+        write!(f, "{} - {}", self.artists.join(", "), self.title)
     } 
 }
 
